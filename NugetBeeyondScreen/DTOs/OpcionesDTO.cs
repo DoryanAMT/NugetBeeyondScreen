50f39205912ef0716d27d34d3b3b6e1599aebddf
Danirolan21 <danirolan21@hotmail.com>
2025-05-30 21:55:49 +0200

    Opciones DTO, Register DTO, KeysModel, UsuarioModel

---
+++ NugetBeeyondScreen/DTOs/OpcionesDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NugetBeeyondScreen.Models;
+
+namespace NugetBeeyondScreen.DTOs
+{
+    public class OpcionesDTO
+    {
+        public List<ComboPeliculas> Peliculas { get; set; }
+        public List<ComboSalas> Salas { get; set; }
+        public List<ComboVersiones> Versiones { get; set; }
+        public List<Evento> Eventos { get; set; }
+    }
+}
---
+++ NugetBeeyondScreen/Models/KeysModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetBeeyondScreen.Models
+{
+    public class KeysModel
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string Iterate { get; set; }
+        public string Salt { get; set; }
+        public string Key { get; set; }
+        public string SecretKey { get; set; }
+        public string MySql { get; set; }
+    }
+}
